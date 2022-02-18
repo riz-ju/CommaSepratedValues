a1794c9920e16b4df9b0d3a9741f2df20c044a30
rizwa <rizwa@DESKTOP-1GNOQEQ>
2022-02-18 21:08:58 +0530

    Add project files.

---
+++ Controllers/CSVController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CSV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CSVController : ControllerBase
+    {
+      
+        FileStream F = new FileStream("sample.csv", FileMode.OpenOrCreate);//, FileAccess.ReadWrite);
+          
+    
+       [HttpGet]
+       public dynamic Get()
+       {
+            StreamReader Reader = new StreamReader(F);
+            return Reader.ReadToEnd();
+          //return data.prrr();
+       }
+/*
+       // GET api/<CSVController>/5
+       [HttpGet("{id}")]
+       public string Get(int id)
+       {
+           return "value";
+       }
+
+       // POST api/<CSVController>
+       [HttpPost]
+       public void Post([FromBody] string value)
+       {
+       }
+
+       // PUT api/<CSVController>/5
+       [HttpPut("{id}")]
+       public void Put(int id, [FromBody] string value)
+       {
+       }
+
+       // DELETE api/<CSVController>/5
+       [HttpDelete("{id}")]
+       public void Delete(int id)
+       {
+       }
+*/
+
+    }
+}
---
+++ data.cs
@@ -0,0 +1,16 @@
+﻿namespace CSV
+{
+    public class data
+    {
+        public static dynamic prrr()
+        {
+            string st = File.ReadAllText("sample.csv");
+            // Console.WriteLine(st);
+            return st;
+            /*FileStream F = new FileStream("sample.csv", FileMode.OpenOrCreate);//, FileAccess.ReadWrite);
+            StreamReader Reader = new StreamReader(F);
+            dynamic str = Reader.ReadToEnd();
+            return str;*/
+        }
+    }
+}
